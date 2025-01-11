bb9e1800665abf5b000d6a20d7b18403eec1df7a
NitionVR <kmaganyane83@gmail.com>
2025-01-11 05:15:56 +0200

    Update Main to WasteManagementApplication main class with Spring Boot configuration

--- src/main/java/com/enviro/assessment/grad001/amosmaganyane/WasteManagementApplication.java
+++ src/main/java/com/enviro/assessment/grad001/amosmaganyane/WasteManagementApplication.java
@@ -1,7 +1,11 @@
 package com.enviro.assessment.grad001.amosmaganyane;
 
-public class Main {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasteManagementApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(WasteManagementApplication.class, args);
     }
 }
