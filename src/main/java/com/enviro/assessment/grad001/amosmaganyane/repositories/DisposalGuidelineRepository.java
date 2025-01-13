8639eada349965dafbf1e94dce5b27c3caa7d858
NitionVR <kmaganyane83@gmail.com>
2025-01-13 03:16:36 +0200

    Add DisposalGuidelineRepository with integration tests

--- src/main/java/com/enviro/assessment/grad001/amosmaganyane/repositories/DisposalGuidelineRepository.java
+++ src/main/java/com/enviro/assessment/grad001/amosmaganyane/repositories/DisposalGuidelineRepository.java
@@ -1,2 +1,10 @@
-package com.enviro.assessment.grad001.amosmaganyane.repositories;public class DisposalGuidelineRepository {
+package com.enviro.assessment.grad001.amosmaganyane.repositories;
+
+import com.enviro.assessment.grad001.amosmaganyane.models.DisposalGuideline;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DisposalGuidelineRepository extends JpaRepository<DisposalGuideline, Long> {
+    // Basic CRUD operations inherited from JpaRepository
 }
--- src/test/java/com/enviro/assessment/grad001/amosmaganyane/repositories/DisposalGuidelineRepositoryTest.java
+++ src/test/java/com/enviro/assessment/grad001/amosmaganyane/repositories/DisposalGuidelineRepositoryTest.java
@@ -1,2 +1,49 @@
-package com.enviro.assessment.grad001.amosmaganyane.repositories;public class DisposalGuidelineRepositoryTest {
+package com.enviro.assessment.grad001.amosmaganyane.repositories;
+
+import com.enviro.assessment.grad001.amosmaganyane.models.DisposalGuideline;
+import com.enviro.assessment.grad001.amosmaganyane.models.WasteCategory;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class DisposalGuidelineRepositoryTest {
+
+    @Autowired
+    private DisposalGuidelineRepository repository;
+
+    @Autowired
+    private WasteCategoryRepository categoryRepository;
+
+    @Test
+    void shouldSaveAndRetrieveDisposalGuideline() {
+        WasteCategory category = categoryRepository.save(
+                new WasteCategory(null, "Recyclable", "Description")
+        );
+
+        DisposalGuideline guideline = new DisposalGuideline(null, "Paper Disposal",
+                "How to dispose paper", category);
+        DisposalGuideline saved = repository.save(guideline);
+
+        assertNotNull(saved.getId());
+        assertTrue(repository.findById(saved.getId()).isPresent());
+        assertEquals("Paper Disposal", saved.getTitle());
+    }
+
+    @Test
+    void shouldDeleteDisposalGuideline() {
+        WasteCategory category = categoryRepository.save(
+                new WasteCategory(null, "Recyclable", "Description")
+        );
+
+        DisposalGuideline guideline = repository.save(
+                new DisposalGuideline(null, "Paper Disposal",
+                        "Instructions", category)
+        );
+
+        repository.deleteById(guideline.getId());
+
+        assertFalse(repository.findById(guideline.getId()).isPresent());
+    }
 }
