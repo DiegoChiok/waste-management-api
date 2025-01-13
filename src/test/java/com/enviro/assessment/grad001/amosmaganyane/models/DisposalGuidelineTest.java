f8495b784c5898ffaecfc7122de61d98b40faf20
NitionVR <kmaganyane83@gmail.com>
2025-01-13 03:08:32 +0200

    Add unit tests for DisposalGuideline model

--- src/test/java/com/enviro/assessment/grad001/amosmaganyane/models/DisposalGuidelineTest.java
+++ src/test/java/com/enviro/assessment/grad001/amosmaganyane/models/DisposalGuidelineTest.java
@@ -1,2 +1,54 @@
-package com.enviro.assessment.grad001.amosmaganyane.models;public class DisposalGuidelineTest {
+package com.enviro.assessment.grad001.amosmaganyane.models;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class DisposalGuidelineTest {
+
+    @Test
+    void testCreateDisposalGuidelineWithValidData() {
+        WasteCategory category = new WasteCategory(1L, "Recyclable", "Description");
+        DisposalGuideline guideline = new DisposalGuideline(5L, "Paper Disposal",
+                "How to dispose paper", category);
+
+        assertEquals(5L, guideline.getId());
+        assertEquals("Paper Disposal", guideline.getTitle());
+        assertEquals("How to dispose paper", guideline.getInstructions());
+        assertEquals(category, guideline.getCategory());
+    }
+
+    @Test
+    void testEqualDisposalGuidelinesShouldBeEqual() {
+        WasteCategory category = new WasteCategory(1L, "Recyclable", "Description");
+        DisposalGuideline guideline1 = new DisposalGuideline(1L, "Paper Disposal",
+                "Instructions", category);
+        DisposalGuideline guideline2 = new DisposalGuideline(1L, "Paper Disposal",
+                "Instructions", category);
+
+        assertEquals(guideline1, guideline2);
+        assertEquals(guideline1.hashCode(), guideline2.hashCode());
+    }
+
+    @Test
+    void testDifferentDisposalGuidelinesShouldNotBeEqual() {
+        WasteCategory category = new WasteCategory(1L, "Recyclable", "Description");
+        DisposalGuideline guideline1 = new DisposalGuideline(1L, "Paper Disposal",
+                "Instructions", category);
+        DisposalGuideline guideline2 = new DisposalGuideline(2L, "Glass Disposal",
+                "Different instructions", category);
+
+        assertNotEquals(guideline1, guideline2);
+        assertNotEquals(guideline1.hashCode(), guideline2.hashCode());
+    }
+
+    @Test
+    void testToString() {
+        WasteCategory category = new WasteCategory(1L, "Recyclable", "Description");
+        DisposalGuideline guideline = new DisposalGuideline(1L, "Paper Disposal",
+                "Instructions", category);
+        String expected = "DisposalGuideline{id=1, title='Paper Disposal', " +
+                "instructions='Instructions', category=" + category + "}";
+
+        assertEquals(expected, guideline.toString());
+    }
 }
